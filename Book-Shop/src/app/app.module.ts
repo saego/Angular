53254156aeeb8ffb56c1b6f03c52b107da251263
saego <usonikru@gmail.com>
2018-10-16 11:59:30 +0300

    mid

--- Book-Shop/src/app/app.module.ts
+++ Book-Shop/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
 import { BookComponent } from './components/book/book.component';
-import { CartComponent } from './components/cart/cart.component';
-import { CartItemComponent } from './components/cart-item/cart-item.component';
+import { CartComponent } from './cart/components/cart/cart.component';
+import { CartItemComponent } from './cart/components/cart-item/cart-item.component';
 import { ProcessOrderFormComponent } from './components/process-order-form/process-order-form.component';
 
 @NgModule({
